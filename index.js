7376818221f21c089f2e7fcba23b0c8da68522ae
Israa Qaba <israa.qaba98@gmail.com>
2022-04-27 04:32:51 +0300

    completed assgiment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+const saturdayFun = (activity =`roller-skate`) => {
+    return(`This Saturday, I want to ${activity}!`);
+}
+saturdayFun(`bathe my dog`);
+
+console.log(saturdayFun());
+console.log(saturdayFun(`bathe my dog`));
+
+
+
+const mondayWork = (plans = `go to the office`) => {
+    return(`This Monday, I will ${plans}.`);
+}
+mondayWork(`work from home`);
+
+console.log(mondayWork());
+console.log(mondayWork(`work from home`));
+
+
+
+
+
+
+
+let wrapAdjective = function(star="*") {
+    return function(sp="special") {
+      return `You are ${star}${sp}${star}!`
+    }
+  }
+
